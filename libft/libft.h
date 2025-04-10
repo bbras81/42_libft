52a5ffff0618db3ecd450f9c9249aad5798c1507
Zedro <zedro@zedro.com>
2025-04-10 12:47:12 +0100

    memcmp

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:25:44 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/04/10 12:12:16 by brunmigu         ###   ########.fr       */
+/*   Updated: 2025/04/10 12:33:18 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 //-------------------------
 // - String.h -
 //-------------------------
