26cada1e234a15bbb2a8c1e91e2fc44d519a25f1
Zedro <zedro@zedro.com>
2025-04-10 16:57:40 +0100

    fucking strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -3,23 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
+/*   By: brunmigu <brunmigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 12:54:18 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/04/10 13:00:55 by brunmigu         ###   ########.fr       */
+/*   Updated: 2025/04/10 16:18:47 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnst(const char *haystack, const char *needle, size_t len)
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    /* check if needle is empty */
+	size_t counter_e;
+	size_t counter_g;
+
+	counter_g = 0;
+	counter_e = 0;
 	if (!*needle)
-        /* return haystack */
 		return ((char *)haystack);
-    /* loop over haystack */
-        /* while current character of haystack is equal to the corresponding
-         * character in needle */
-             /* check if we have the complete needle */
+	while (haystack[counter_g] && counter_g < len)
+	{
+	    while (haystack[counter_g + counter_e] == needle[counter_e] && haystack[counter_g + counter_e] && counter_g + counter_e < len)
+		{
+			counter_e++;
+			if (needle[counter_e] == 0)
+                return ((char *) haystack + counter_g);
+		}
+		counter_g++;
+		counter_e = 0;
+	}
+	return (NULL);
 }
+/*
+#include <stdio.h>
+int main()
+{
+	char *str = "lorem ipsum dolor sit amet";
+	printf("%s\n", ft_strnstr(str, "lorem",15));
+}*/
