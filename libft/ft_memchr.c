328eb60c3358e16008facd856129d9a5477fa1f2
Zedro <zedro@zedro.com>
2025-04-10 12:21:42 +0100

    memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,31 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 22:40:51 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/04/09 23:09:33 by brunmigu         ###   ########.fr       */
+/*   Created: 2025/04/10 11:55:06 by brunmigu          #+#    #+#             */
+/*   Updated: 2025/04/10 12:20:15 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+void *ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	counter;
+	size_t counter;
+	unsigned char *str;
+	unsigned char chr;
 
+	str = (unsigned char *) s;
+	chr = (unsigned char) c;
 	counter = 0;
-	while (counter < n)
-	{
-		if ((unsigned char)s1[counter] != (unsigned char)s2[counter])
-		{
-			return ((unsigned char)s1[counter] - (unsigned char)s2[counter]);
-		}
-		if (s1[counter] == '\0')
-			return (0);
-		counter++;
+	while (counter < n) {
+		if (str[counter] == chr)
+			return ((void *)&str[counter]);
+		counter ++;
 	}
-	return (0);
+	return (NULL);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:25:44 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/04/09 17:02:13 by brunmigu         ###   ########.fr       */
+/*   Updated: 2025/04/10 12:12:16 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,13 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 //-------------------------
 // - String.h -
 //-------------------------
 char	*ft_strchr(const char *str, int chr);
-size_t		ft_strlen(const char *str);
+size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dsize);
-char *ft_strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
