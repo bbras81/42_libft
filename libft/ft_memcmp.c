b1f85fb66d11128c40db6a185227c7da90968e42
Bruno Brás <miguelazevedodg@gmail.com>
2025-04-10 12:48:37 +0100

    norminette

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -12,20 +12,20 @@
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t counter;
-	unsigned char *str1;
-	unsigned char *str2;
+	size_t			counter;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	counter = 0;
-    str1 = (unsigned char *) s1;
-    str2 = (unsigned char *) s2;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	if (n == 0)
-		return(0);
-	while(counter < n)
+		return (0);
+	while (counter < n)
 	{
-		if(str1[counter] != str2[counter])
+		if (str1[counter] != str2[counter])
 			return (str1[counter] - str2[counter]);
 		counter++;
 	}
