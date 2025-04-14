08347b10605c2cffb082e8ef4a1ef3c3a88dd2e4
Bruno Brás <miguelazevedodg@gmail.com>
2025-04-14 10:33:40 +0100

    strtrim backup

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+#include <stddef.h>
 
 int	trim(const char *set, char c)
 {
@@ -33,35 +33,35 @@ char	*ft_strtrim(const char *s1, const char *set)
 	size_t	counter_start;
 	size_t	counter_end;
 	size_t	counter;
+	size_t	len;
 
 	counter_start = 0;
-	counter_end = ft_strlen(s1);
-	while (s1[counter_start])
-	{
-		if (trim(set, s1[counter_start]))
-			break ;
+	counter_end = ft_strlen(s1) - 1;
+	while (trim(set, s1[counter_start]))
 		counter_start++;
-	}
-	while (counter_end > counter_start)
-	{
-		if (trim(set, s1[counter_end]))
-			break ;
-	}
-	ret = ft_calloc((counter_end - counter_start) + 1, sizeof(char));
+	while (trim(set, s1[counter_end]))
+		counter_end--;
+	len = counter_end - (counter_start - 1);
+	ret = ft_calloc(len + 1, sizeof(char));
+	if (!ret)
+		return (NULL);
 	counter = 0;
 	while (counter_start <= counter_end)
 	{
 		ret[counter] = s1[counter_start];
 		counter++;
+		counter_start++;
 	}
 	ret[counter_start] = 0;
 	return (ret);
 }
+/*
+#include <stdio.h>
 
 int	main(void)
 {
 	char	*str;
 
-	str = "ababaaaMy name is Simonbbaaabbad";
-	printf("%s", ft_strtrim(str, "ab"));
-}
+	str = "  \t \t \n   \n\n\n\t";
+	printf("%s", ft_strtrim(str, ""));
+}*/
