9166056f5bc0575092ba94cd70a4a36974273ee3
Zedro <zedro@zedro.com>
2025-04-15 17:18:00 +0100

    itoa

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:25:44 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/04/13 15:48:00 by brunmigu         ###   ########.fr       */
+/*   Updated: 2025/04/15 17:17:02 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,3 +51,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
