8292570a25ee228d1d3e7564abf1a0d29267fa92
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2025-07-11 13:43:55 +0100

    Auto-commit: 2025-07-11 13:43:55

--- ft_libft/ft_lstnew.c
+++ ft_libft/ft_lstnew.c
@@ -5,21 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: brunmigu <brunmigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/17 13:36:19 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/07/10 22:22:35 by brunmigu         ###   ########.fr       */
+/*   Created: 2025/07/10 22:24:57 by brunmigu          #+#    #+#             */
+/*   Updated: 2025/07/10 22:30:09 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
+
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*new_list;
+	t_list	*new;
 
-	new_list = malloc(sizeof(t_list));
-	if (!new_list)
+	new = ft_calloc(1, sizeof(t_list));
+	if (!new)
 		return (NULL);
-	new_list->content = content;
-	new_list->next = NULL;
-	return (new_list);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
